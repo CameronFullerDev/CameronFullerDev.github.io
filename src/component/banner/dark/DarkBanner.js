622e3e3711c97fd5e6161fc543f65a18fe350a48
Cameron Fuller <cameron-fuller@hotmail.com>
2024-03-21 17:17:29 +0000

    Format

--- src/component/banner/dark/DarkBanner.js
+++ src/component/banner/dark/DarkBanner.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Container, Row, Col, Jumbotron, Button } from 'react-bootstrap'
-import ReactRotatingText from 'react-rotating-text'
-import './DarkBanner.css'
-
+import React from "react";
+import { Container, Row, Col, Jumbotron, Button } from "react-bootstrap";
+import ReactRotatingText from "react-rotating-text";
+import "./DarkBanner.css";
 
 function DarkBanner(props) {
-    return (
-        <Container fluid className="banner dark-bg page">
-            <Row className="justify-content-center">
-                <Col md={4} style={{ position: "relative", bottom: "-30vh", color: "white"}}>
-                    <h1 className="text-center"><ReactRotatingText items={props.Text} /></h1>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container fluid className="banner dark-bg page">
+      <Row className="justify-content-center">
+        <Col
+          md={4}
+          style={{ position: "relative", bottom: "-30vh", color: "white" }}
+        >
+          <h1 className="text-center">
+            <ReactRotatingText items={props.Text} />
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default DarkBanner;
--- src/component/banner/light/LightBanner.js
+++ src/component/banner/light/LightBanner.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap'
-import './LightBanner.css'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./LightBanner.css";
 
 function LightBanner(props) {
-    return (
-        <Container fluid className="banner light-bg">
-            <Row className="justify-content-center" style={{ position: "relative", bottom: "-10vh" }}>
-                <h1>{props.Heading}</h1>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container fluid className="banner light-bg">
+      <Row
+        className="justify-content-center"
+        style={{ position: "relative", bottom: "-10vh" }}
+      >
+        <h1>{props.Heading}</h1>
+      </Row>
+    </Container>
+  );
 }
 
 export default LightBanner;
