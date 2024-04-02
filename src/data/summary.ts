2929f02411d8dd2cc85d5252768cd3c7969107ab
Cameron Fuller <cameron-fuller@hotmail.com>
2024-04-02 13:32:13 +0100

    Content updates

--- src/data/summary.ts
+++ src/data/summary.ts
@@ -1,9 +1,6 @@
 const shortSummary =
-  "Experienced software engineer with expertise in full-stack development using the .NET stack, Vue JS, and Microsoft Azure.";
+  "Experienced software engineer with expertise in development using .NET, Vue JS, and Azure.";
 
-const summary =
-  shortSummary +
-  "Passionate about solving complex problems and delivering efficient, user-centric software solutions." +
-  "Always seeking new opportunities for personal development and to make a difference in the companies I work for.";
+const summary = `${shortSummary} Passionate about solving complex problems and delivering efficient, user-centric software solutions. Always seeking new opportunities for personal development and to make a difference in the companies I work for.`;
 
 export { shortSummary, summary };
